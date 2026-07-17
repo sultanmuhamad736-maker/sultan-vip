7bce92edb427c8ea4d873635836cc35b0a8b47bf
sultanmuhamad736-maker <sultanmuhamad736@gmail.com>
2026-07-17 23:15:33 +0300

    Remove installation script for sultan-vip

--- install-mini.sh
+++ install-mini.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-set -e
-
-REPO="https://raw.githubusercontent.com/sultanmuhamad736-maker/sultan-vip/main"
-MAIN="sultan-vip.sh"
-
-if [ "$(id -u)" != "0" ]; then
-  echo "Run as root."
-  exit 1
-fi
-
-apt update -y
-apt install -y curl bash
-
-curl -fL -o /tmp/sultan-vip.sh "$REPO/$MAIN"
-chmod +x /tmp/sultan-vip.sh
-bash /tmp/sultan-vip.sh
+ /tmp/sultan-vip.sh
